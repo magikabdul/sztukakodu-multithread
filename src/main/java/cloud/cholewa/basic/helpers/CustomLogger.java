decc45b1b8359b25df3960f0aa7217d4f6624716
Krzysztof Cholewa <krzysztof.cholewa@gmail.com>
2022-03-15 13:30:45 +0100

    added custom logger implementation

---
+++ src/main/java/cloud/cholewa/basic/helpers/CustomLogger.java
@@ -0,0 +1,61 @@
+package cloud.cholewa.basic.helpers;
+
+import java.sql.Time;
+import java.sql.Timestamp;
+import java.time.Instant;
+
+public class CustomLogger {
+
+    private final int MAX_BLOCK_LENGTH = 30;
+
+    private final String className;
+    private final String threadName;
+
+
+    public CustomLogger(String className, String threadName) {
+        this.className = className;
+        this.threadName = threadName;
+    }
+
+    public void startMessage() {
+        System.out.println(header() + "Starting...");
+    }
+
+    public void endMessage() {
+        System.out.println(header() + "Done!");
+    }
+
+    public void message(String message) {
+        System.out.println(header() + message);
+    }
+
+    private String header() {
+        return " " + getTime() + getBlock("Class", className) + getBlock("Thread", threadName) + "- ";
+    }
+
+    private String getBlock(String title, String value) {
+        StringBuilder sb = new StringBuilder();
+
+        sb.append("[ ")
+                .append(title)
+                .append(": ")
+                .append(value);
+
+        while (sb.length() < MAX_BLOCK_LENGTH) {
+            sb.append(" ");
+        }
+
+        sb.append(" ] ");
+        return sb.toString();
+    }
+
+    private String getTime() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(Timestamp.from(Instant.now()));
+
+        while (sb.length() < MAX_BLOCK_LENGTH) {
+            sb.append(" ");
+        }
+        return sb.toString();
+    }
+}
